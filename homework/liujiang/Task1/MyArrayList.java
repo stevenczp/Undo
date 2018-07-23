16214e75b8b8c34780a74802c019cf8e6b78780d
qmylzx <qmylzx@outlook.com>
2018-07-24 00:11:50 +0800

    Test

---
+++ homework/liujiang/Task1/MyArrayList.java
@@ -0,0 +1,110 @@
+package com.cc.test.learn;
+
+import java.util.Arrays;
+
+/*
+ *下标从0开始 
+ * */
+public class MyArrayList<T> implements MyList<T> {
+	private T[] list;
+	private int size = 0;
+	private static final int initcapacity = 10;
+	private int capacity = initcapacity;
+
+	MyArrayList() {
+		this(initcapacity);
+	}
+
+	MyArrayList(int capacity) {
+		this.capacity = capacity;
+		if (capacity > 0) {
+			list = (T[]) new Object[capacity];
+		} else if (capacity == 0) {
+			list = null;
+		} else {
+			throw new IllegalArgumentException("容量输入错误！");
+		}
+	}
+
+	public void add(T element) {
+		if (size < capacity) {
+			list[size++] = element;
+		} else {// 扩容
+			grow(size);
+			list[size++] = element;
+		}
+	}
+
+	@Override
+	public T get(int index) {
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		return list[index];
+	}
+
+	@Override
+	public T set(int index, T element) {
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		list[index] = element;
+		return list[index];
+	}
+
+	@Override
+	public T remove(int index) {
+		if (size == 0) {
+			throw new IllegalArgumentException("list为空！");
+		}
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		T temp = list[index];
+		int t = --size;
+		for (int i = index; i < t; i++) {
+			list[i] = list[i + 1];
+			// System.out.println(toString());
+		}
+
+		return temp;
+	}
+
+	@Override
+	public void clear() {
+		size = 0;
+		capacity = 0;
+		list = null;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	private void grow(int minCapacity) {
+		if (list == null) {
+			list = (T[]) new Object[initcapacity];
+			capacity = initcapacity;
+		} else {
+			list = Arrays.copyOf(list, minCapacity * 2);
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < size; i++) {
+			sb.append(list[i].toString() + " ");
+		}
+		return sb.toString();
+	}
+}
---
+++ homework/liujiang/Task1/MyLinkedList.java
@@ -0,0 +1,140 @@
+package com.cc.test.learn;
+/*
+ *下标从0开始 
+ * */
+public class MyLinkedList<T> implements MyList<T> {
+	private int size = 0;
+	private final Node head = new Node();
+	private Node current = head;
+
+	public MyLinkedList() {
+	}
+
+	@Override
+	public void add(T element) {
+		Node temp = new Node();
+		temp.setData(element);
+		current.setNext(temp);
+		temp.setPre(current);
+		current = temp;
+		++size;
+	}
+
+	@Override
+	public T get(int index) {
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		Node temp = head;
+		for(int i=0;i<=index;i++){
+			temp = temp.getNext();
+		}
+		return temp.getData();
+	}
+
+	@Override
+	public T set(int index, T element) {
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		Node temp = head;
+		for(int i=0;i<=index;i++){
+			temp = temp.getNext();
+		}
+		temp.setData(element);
+		return temp.getData();
+	}
+
+	@Override
+	public T remove(int index) {
+		if (size == 0) {
+			throw new IllegalArgumentException("list为空！");
+		}
+		if (index >= size || index < 0) {
+			throw new IllegalArgumentException("指针越界！");
+		}
+		Node temp;
+		if(index==size-1){
+			temp = current;
+			current = current.getPre();
+			current.setNext(null);
+			temp.setPre(null);
+		}else{
+			temp = head;
+			Node previous;
+			for(int i=0;i<=index;i++){
+				temp = temp.getNext();
+			}
+			previous = temp.getPre();
+			previous.setNext(temp.getNext());
+			temp.getNext().setPre(previous);
+			temp.setNext(null);
+			temp.setPre(null);
+		}
+		--size;
+		return temp.getData();
+	}
+
+	@Override
+	public void clear() {
+		head.setData(null);
+		head.setNext(null);
+		head.setPre(null);
+		size = 0;
+	}
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		Node temp = head;
+		for (int i = 0; i < size; i++) {
+			temp = temp.getNext();
+			sb.append(temp.getData() + " ");
+		}
+		
+		return sb.toString();
+	}
+
+	class Node {
+		private T data = null;
+		private Node pre= null;
+		private Node next= null;
+
+		public Node getPre() {
+			return pre;
+		}
+
+		public void setPre(Node pre) {
+			this.pre = pre;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+	}
+}
